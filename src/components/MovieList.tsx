97d943c5c61ee152367b00363c9c963d365032df
pvt12 <pvt12@gcloud.ua.es>
2026-02-22 16:38:47 +0100

    Add movie to movie context and print it

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,8 +1,11 @@
-import { useSelector } from 'react-redux'; // Obtener datos del estado
-import type { RootState } from '../store/store';
+import { useDispatch, useSelector } from 'react-redux'; // Obtener datos del estado
+import type { AppDispatch, RootState } from '../store/store';
+import { addMovie } from '../store/slices/movieSlice';
 
 export const MovieList = () => {
-    const movies = useSelector((state: RootState) => state.movies.movies);
+    const movies = useSelector((state: RootState) => state.movies.movies); // Para obtener el listado de peliculas
+    const dispatch = useDispatch<AppDispatch>(); // para realizar una accion;
+    const newMovie = { id: 3, name: 'Otra peli' };
     console.log(movies);
     return (
         <>
@@ -10,6 +13,7 @@ export const MovieList = () => {
             {movies.map((movie) => (
                 <div key={movie.id}>{movie.name}</div>
             ))}
+            <button onClick={() => dispatch(addMovie(newMovie))}></button >
         </>
     )
 }
